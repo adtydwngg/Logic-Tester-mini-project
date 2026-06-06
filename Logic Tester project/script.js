e5435a0d578e525eefa96eddd73933e6dedbaf9b
adtydwngg <adityadewangga86@gmail.com>
2026-06-07 02:35:02 +0700

    Add files via upload

---
+++ Logic Tester project/script.js
@@ -0,0 +1,10 @@
+ 
+            console.log("AND", andResult);
+            console.log("OR", orResult);
+            console.log("NOT", notA);
+            console.log("NAND", andResult);
+            console.log("NOR", norResult);
+            console.log("XOR", xorResult);
+            console.log("XNOR", xnorResult);
+
+
